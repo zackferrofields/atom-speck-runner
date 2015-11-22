7fa13400422a9ef93e927d7315dba59f215789fe
Zack Ferro-Fields <zackferrofields@gmail.com>
2015-11-22 19:44:41 +0000

    tape stream atom.notifications

--- examples/sum.js
+++ examples/sum.js
@@ -1,7 +1,7 @@
 module.exports = {
   // test > sum function
   // # sum(1,2) === 3 (returns the sum of both parameters)
-  // # sum(3,-1) === 2 (works with negative numbers)
+  // # sum(3,-1) === 1 (works with negative numbers)
   sum: function(a,b) {
     return a + b;
   }
