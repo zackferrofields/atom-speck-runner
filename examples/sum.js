3302874270a828ef341268f8973c2098912c8736
Zack Ferro Fields <zackferrofields@gmail.com>
2015-11-25 12:53:31 +0000

    run custom tape logger

--- examples/sum.js
+++ examples/sum.js
@@ -1,7 +1,6 @@
 module.exports = {
   // test > sum function
   // # sum(1,2) === 3 (returns the sum of both parameters)
-  // # sum(3,-1) === 1 (works with negative numbers)
   sum: function(a,b) {
     return a + b;
   }
