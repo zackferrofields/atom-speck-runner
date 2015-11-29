8967da957287bac5f69cc0e04631a4695db96a80
Zack Ferro-Fields <zackferrofields@gmail.com>
2015-11-29 12:45:42 +0000

    Highlight errors

--- examples/sum.js
+++ examples/sum.js
@@ -1,8 +1,18 @@
+// test > sum function
+// # sum(1 ,2) === 3 (returns the sum of both parameters)
+// # sum(3, -1) === -2 (works with negative numbers)
+function sum(a,b) {
+  return a + b;
+}
+
 module.exports = {
-  // test > sum function
-  // # sum(1,2) === 3 (returns the sum of both parameters)
-  // # sum(3,-1) === 2 (works with negative numbers)
-  sum: function(a,b) {
+  sum: sum,
+  /*
+   test > add function
+   # add(1, 2) == 3 (returns the sum of both params)
+   # add(3, 4) == -7 (returns the sum of both params)
+  */
+  add: function(a,b) {
     return a + b;
   }
-}
+};
